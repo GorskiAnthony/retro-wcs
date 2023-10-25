5222862bdde90dc5c1fc9eede86ccc48c19c81e1
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-25 16:59:31 +0200

    save card in columns

---
+++ src/components/Cards/page.tsx
@@ -0,0 +1,28 @@
+interface CardProps {
+	onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+	setIsNew: React.Dispatch<React.SetStateAction<boolean>>;
+	card: string[];
+}
+
+import style from "./cards.module.css";
+
+function Card({ onSubmit, setIsNew, card }: CardProps) {
+	return (
+		<form className={style.card} onSubmit={onSubmit}>
+			<textarea name="card" id="card"></textarea>
+			<div className={style.buttons}>
+				<button className={style.buttonSave} type="submit">
+					Save
+				</button>
+				<button
+					className={style.buttonError}
+					onClick={() => setIsNew(false)}
+				>
+					Close
+				</button>
+			</div>
+		</form>
+	);
+}
+
+export default Card;
--- src/components/Columns/page.tsx
+++ src/components/Columns/page.tsx
@@ -1,18 +1,49 @@
+"use client";
+
 interface ColumnsProps {
 	title: string;
 	color: string;
 }
 
+import { useState } from "react";
+import Card from "../Cards/page";
+
 import style from "./columns.module.css";
 function Columns({ title, color }: ColumnsProps) {
+	const [cards, setCards] = useState<string[]>([]);
+	const [isNew, setIsNew] = useState<boolean>(false);
+
+	function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		setIsNew(false);
+		setCards([...cards, e.currentTarget.card.value]);
+	}
+
+	function addCard() {
+		setIsNew(true);
+	}
+
 	return (
 		<div className={style.column}>
 			<h2 style={{ backgroundColor: color }} className={style.h2}>
 				{title}
 			</h2>
-			<button style={{ color: color }} className={`${style.button}`}>
+
+			<button
+				style={{ color: color }}
+				className={`${style.button}`}
+				onClick={addCard}
+			>
 				+ Add New Card
 			</button>
+			{isNew && (
+				<Card onSubmit={onSubmit} setIsNew={setIsNew} card={cards} />
+			)}
+			{cards.map((card, index) => (
+				<div key={index} className={style.card}>
+					{card}
+				</div>
+			))}
 		</div>
 	);
 }
