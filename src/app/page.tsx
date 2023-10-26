cde4b5d44f7dfc87954fe717aa4993dc7ce01545
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-26 16:42:52 +0200

    change import { io } to io

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 
 import Links from "../components/Links/page";
 import Columns from "../components/Columns/columns";
