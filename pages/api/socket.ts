0c4b59a8e37c90078c92e5280b50956c2f27d487
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-11-01 14:48:20 +0100

    add unique room

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,5 +1,11 @@
 import { Server } from "socket.io";
 
+interface MessageObject {
+	message: string;
+	column: string;
+	room: string; // Ajoutez la propriété 'room'
+}
+
 export default function handler(req: any, res: any) {
 	if (res.socket.server.io) {
 		console.log("already have io instance");
@@ -9,16 +15,23 @@ export default function handler(req: any, res: any) {
 
 		io.on("connection", (socket) => {
 			console.log("Socket.io server started: ", socket.id);
-			socket.on("addMessage", (obj: Object) => {
+
+			socket.on("joinRoom", (slug) => {
+				socket.join(slug);
+				console.log(`Socket ${socket.id} joined room ${slug}`);
+			});
+
+			socket.on("addMessage", (obj: MessageObject) => {
 				/**
 				 * output:
 				 *
 				 * obj = {
 				 * 	"message": "Hello World",
 				 *  "column": "good",
+				 *  "room": "room1"
 				 * };
 				 */
-				io.emit("messageAdded", obj);
+				io.to(obj.room).emit("messageAdded", obj);
 			});
 		});
 	}
