583f949ae2f635fdd072e03b3692dd173e06d390
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-11-01 20:02:18 +0100

    update css remove dark theme

--- src/components/Links/page.tsx
+++ src/components/Links/page.tsx
@@ -67,7 +67,7 @@ function Links() {
 				id="navbar-default"
 			>
 				<div className="mx-5">
-					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
+					<ul className="font-medium flex flex-col p-4 md:px-5 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
 						{links.map((link) => (
 							<li key={link.id} className="py-2">
 								<Link
